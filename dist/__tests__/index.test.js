3342ef6a568d17aad0a7f6a845be472390461bdc
Ethan Jon <ethanczahor@gmail.com>
2020-07-13 10:03:20 -0700

    v1.0.8

--- dist/__tests__/index.test.js
+++ dist/__tests__/index.test.js
@@ -1,44 +1,43 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const __1 = require('../')
-const { SPREADSHEET_ID, CREDS } = process.env
-let easySheets
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const __1 = require("../");
+const { SPREADSHEET_ID, CREDS } = process.env;
+let easySheets;
 beforeAll(() => {
-  easySheets = new __1.default(SPREADSHEET_ID || '', CREDS || '')
-})
+    easySheets = new __1.default(SPREADSHEET_ID || '', CREDS || '');
+});
 test('all operations', async () => {
-  // multisheet
-  for (const sheet of [undefined, 'Sheet2']) {
-    // clearRange
-    expect(await easySheets.clearRange('A1:B5000000', { sheet })).toBe(true)
-    // updateRange
-    expect(
-      await easySheets.updateRange(
-        'A1:B2',
-        [
-          ['First Name', 'Last Name'],
-          ['Tim', 'Jones'],
-        ],
-        { sheet },
-      ),
-    ).toBe(true)
-    // addRow
-    expect(await easySheets.addRow(['Bob', 'Smith'], { sheet })).toBe(true)
-    // getRange
-    expect(await easySheets.getRange('A1:B3', { sheet })).toStrictEqual([
-      ['First Name', 'Last Name'],
-      ['Tim', 'Jones'],
-      ['Bob', 'Smith'],
-    ])
-    // getRange with headerRow raw
-    expect(await easySheets.getRange('A1:B3', { headerRow: 'raw', sheet })).toStrictEqual([
-      { 'First Name': 'Tim', 'Last Name': 'Jones' },
-      { 'First Name': 'Bob', 'Last Name': 'Smith' },
-    ])
-    // getRange with headerRow
-    expect(await easySheets.getRange('A1:B3', { headerRow: true, sheet })).toStrictEqual([
-      { firstName: 'Tim', lastName: 'Jones' },
-      { firstName: 'Bob', lastName: 'Smith' },
-    ])
-  }
-})
+    // multisheet
+    for (const sheet of [undefined, 'Sheet2']) {
+        // clearRange
+        expect(await easySheets.clearRange('A1:B5000000', { sheet })).toBe(true);
+        // updateRange
+        expect(await easySheets.updateRange('A1:B2', [
+            ['First Name', 'Last Name'],
+            ['Tim', 'Jones'],
+        ], { sheet })).toBe(true);
+        // addRow
+        expect(await easySheets.addRow(['Bob', 'Smith'], { sheet })).toBe(true);
+        // add multiple rows
+        expect(await easySheets.addMultipleRows([
+            ['Bob', 'Smith'],
+            ['John', 'Doe'],
+        ], { sheet })).toBe(true);
+        // getRange
+        expect(await easySheets.getRange('A1:B3', { sheet })).toStrictEqual([
+            ['First Name', 'Last Name'],
+            ['Tim', 'Jones'],
+            ['Bob', 'Smith'],
+        ]);
+        // getRange with headerRow raw
+        expect(await easySheets.getRange('A1:B3', { headerRow: 'raw', sheet })).toStrictEqual([
+            { 'First Name': 'Tim', 'Last Name': 'Jones' },
+            { 'First Name': 'Bob', 'Last Name': 'Smith' },
+        ]);
+        // getRange with headerRow
+        expect(await easySheets.getRange('A1:B3', { headerRow: true, sheet })).toStrictEqual([
+            { firstName: 'Tim', lastName: 'Jones' },
+            { firstName: 'Bob', lastName: 'Smith' },
+        ]);
+    }
+}, 60000);
--- dist/index.js
+++ dist/index.js
@@ -1,77 +1,86 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const googleapis_1 = require('googleapis')
-const lodash_1 = require('lodash')
-const util_1 = require('util')
-const buildRange = (range, sheet) => (sheet ? `${sheet}!${range}` : range)
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const googleapis_1 = require("googleapis");
+const lodash_1 = require("lodash");
+const util_1 = require("util");
+const buildRange = (range, sheet) => (sheet ? `${sheet}!${range}` : range);
 class EasySheets {
-  constructor(spreadsheetId, creds64) {
-    this.addRow = async (values, opts = {}) => {
-      const sheets = await this.authorize()
-      await sheets.spreadsheets.values.append({
-        range: buildRange('A1:A5000000', opts.sheet),
-        requestBody: { values: [values] },
-        spreadsheetId: this.spreadsheetId,
-        valueInputOption: 'USER_ENTERED',
-      })
-      return true
+    constructor(spreadsheetId, creds64) {
+        this.addRow = async (values, opts = {}) => {
+            const sheets = await this.authorize();
+            await sheets.spreadsheets.values.append({
+                range: buildRange('A1:A5000000', opts.sheet),
+                requestBody: { values: [values] },
+                spreadsheetId: this.spreadsheetId,
+                valueInputOption: 'USER_ENTERED',
+            });
+            return true;
+        };
+        this.addMultipleRows = async (values, opts = {}) => {
+            const sheets = await this.authorize();
+            await sheets.spreadsheets.values.append({
+                range: buildRange('A1:A5000000', opts.sheet),
+                requestBody: { values: values },
+                spreadsheetId: this.spreadsheetId,
+                valueInputOption: 'USER_ENTERED',
+            });
+            return true;
+        };
+        this.authorize = async () => {
+            if (!this.sheets) {
+                const oauth2Client = new googleapis_1.google.auth.JWT({
+                    email: this.serviceAccountCreds.client_email,
+                    key: this.serviceAccountCreds.private_key,
+                    scopes: ['https://spreadsheets.google.com/feeds'],
+                });
+                const authorize = util_1.promisify(oauth2Client.authorize).bind(oauth2Client);
+                await authorize();
+                this.sheets = googleapis_1.google.sheets({
+                    auth: oauth2Client,
+                    version: 'v4',
+                });
+            }
+            return this.sheets;
+        };
+        this.clearRange = async (range, opts = {}) => {
+            const sheets = await this.authorize();
+            await sheets.spreadsheets.values.clear({
+                range: buildRange(range, opts.sheet),
+                spreadsheetId: this.spreadsheetId,
+            });
+            return true;
+        };
+        this.getRange = async (range, opts = {}) => {
+            const sheets = await this.authorize();
+            const { data: { values }, } = await sheets.spreadsheets.values.get({
+                range: buildRange(range, opts.sheet),
+                spreadsheetId: this.spreadsheetId,
+            });
+            if (opts.headerRow && values) {
+                const headerKeys = opts.headerRow === 'raw' ? values[0] : values[0].map(lodash_1.camelCase);
+                return values.slice(1).map((row) => {
+                    return headerKeys.reduce((obj, header, i) => {
+                        obj[header] = row[i];
+                        return obj;
+                    }, {});
+                });
+            }
+            else {
+                return values;
+            }
+        };
+        this.updateRange = async (range, values, opts = {}) => {
+            const sheets = await this.authorize();
+            await sheets.spreadsheets.values.update({
+                range: buildRange(range, opts.sheet),
+                requestBody: { values },
+                spreadsheetId: this.spreadsheetId,
+                valueInputOption: 'USER_ENTERED',
+            });
+            return true;
+        };
+        this.spreadsheetId = spreadsheetId;
+        this.serviceAccountCreds = JSON.parse(Buffer.from(creds64, 'base64').toString());
     }
-    this.authorize = async () => {
-      if (!this.sheets) {
-        const oauth2Client = new googleapis_1.google.auth.JWT({
-          email: this.serviceAccountCreds.client_email,
-          key: this.serviceAccountCreds.private_key,
-          scopes: ['https://spreadsheets.google.com/feeds'],
-        })
-        const authorize = util_1.promisify(oauth2Client.authorize).bind(oauth2Client)
-        await authorize()
-        this.sheets = googleapis_1.google.sheets({
-          auth: oauth2Client,
-          version: 'v4',
-        })
-      }
-      return this.sheets
-    }
-    this.clearRange = async (range, opts = {}) => {
-      const sheets = await this.authorize()
-      await sheets.spreadsheets.values.clear({
-        range: buildRange(range, opts.sheet),
-        spreadsheetId: this.spreadsheetId,
-      })
-      return true
-    }
-    this.getRange = async (range, opts = {}) => {
-      const sheets = await this.authorize()
-      const {
-        data: { values },
-      } = await sheets.spreadsheets.values.get({
-        range: buildRange(range, opts.sheet),
-        spreadsheetId: this.spreadsheetId,
-      })
-      if (opts.headerRow && values) {
-        const headerKeys = opts.headerRow === 'raw' ? values[0] : values[0].map(lodash_1.camelCase)
-        return values.slice(1).map((row) => {
-          return headerKeys.reduce((obj, header, i) => {
-            obj[header] = row[i]
-            return obj
-          }, {})
-        })
-      } else {
-        return values
-      }
-    }
-    this.updateRange = async (range, values, opts = {}) => {
-      const sheets = await this.authorize()
-      await sheets.spreadsheets.values.update({
-        range: buildRange(range, opts.sheet),
-        requestBody: { values },
-        spreadsheetId: this.spreadsheetId,
-        valueInputOption: 'USER_ENTERED',
-      })
-      return true
-    }
-    this.spreadsheetId = spreadsheetId
-    this.serviceAccountCreds = JSON.parse(Buffer.from(creds64, 'base64').toString())
-  }
 }
-exports.default = EasySheets
+exports.default = EasySheets;
